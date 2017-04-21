8a9fd809bacf32ba6241fd7f6b623beebde4fc56
kate-moroz <kate.e.moroz@gmail.com>
2017-04-21 14:35:07 +0300

    Created class for group managing

--- test/group_test.py
+++ test/group_test.py
@@ -14,12 +14,12 @@ def app(request):
 
 def test_group_test(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="dfsdfsd", header="sdfvcv", footer="dsfwfwe"))
+    app.group.create(Group(name="dfsdfsd", header="sdfvcv", footer="dsfwfwe"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
