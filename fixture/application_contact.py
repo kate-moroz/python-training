23749c8b9f5a9c3e479c64ccabeab9d67b4c05f7
kate-moroz <kate.e.moroz@gmail.com>
2017-04-21 19:02:34 +0300

    Created class for session management in new contact test

--- fixture/application_contact.py
+++ fixture/application_contact.py
@@ -1,13 +1,12 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session2 import SessionHelper
 
 class Application_contact:
+
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        self.session = SessionHelper(self)
 
     def create_contact(self, contact):
         wd = self.wd
@@ -81,17 +80,6 @@ class Application_contact:
         wd.find_element_by_name("notes").send_keys(contact.notes)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_homepage()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_homepage(self):
         wd = self.wd
