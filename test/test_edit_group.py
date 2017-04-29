0f59a53bf838156bee69fa38fefa15fdb762851e
kate-moroz <kate.e.moroz@gmail.com>
2017-04-29 16:05:26 +0300

    Added fixture validation before each test
    + singled out login, logout

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,6 +3,4 @@ from model.contact import Contact
 
 
 def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group()
-    app.session.logout()
