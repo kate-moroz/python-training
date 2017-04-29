94d75cad4071dd642f314bbd4d5c69b7586364b3
kate-moroz <kate.e.moroz@gmail.com>
2017-04-29 20:59:38 +0300

    Modify_group_test validation
    Checks if there are any groups available to modification

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(fname = "test", mname = "test1", lname = "test2"))
+        app.contact.create(Contact(fname = "test", mname = "test1", lname="test2"))
     app.contact.delete_first_contact()
 
 
