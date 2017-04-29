df296e430713681d2ed2778194126d9ff6b1ddea
kate-moroz <kate.e.moroz@gmail.com>
2017-04-29 18:54:13 +0300

    test_del_contact for fix

--- model/contact.py
+++ model/contact.py
@@ -1,24 +1,9 @@
 class Contact:
 
-    def __init__(self, fname=None, mname=None, lname=None, nname=None, title=None, company=None, address1=None, homenum=None, mobnum=None, worknum=None, fax=None, email1=None, email2=None, email3=None, homepage=None, birthyear=None, anniversary=None, address2=None, notes=None):
+    def __init__(self, fname=None, mname=None, lname=None):
         self.fname = fname
         self.mname = mname
         self.lname = lname
-        self.nname = nname
-        self.title = title
-        self.company = company
-        self.address1 = address1
-        self.homenum = homenum
-        self.mobnum = mobnum
-        self.worknum = worknum
-        self.fax = fax
-        self.email1 = email1
-        self.email2 = email2
-        self.email3 = email3
-        self.homepage = homepage
-        self.birthyear = birthyear
-        self.anniversary = anniversary
-        self.address2 = address2
-        self.notes = notes
+
 
 
