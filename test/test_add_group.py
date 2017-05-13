0bf2f0a5dea161f34f681298d56266d3f6747c59
kate-moroz <kate.e.moroz@gmail.com>
2017-05-13 22:27:34 +0300

    Added group lists comparison

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="dfsdfsd", header="sdfvcv", footer="dsfwfwe"))
+    group = Group(name="dfsdfsd", header="sdfvcv", footer="dsfwfwe")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-    app.session.logout()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="dfsdfsd", header="sdfvcv", footer="dsfwfwe")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-    app.session.logout()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
